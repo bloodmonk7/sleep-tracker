a53aac023a4a8d7de94677e57a4f7f2615e15b5d
bloodmonk7 <rritikssingh@gmail.com>
2023-10-27 13:39:52 +0530

    add bcrypt for password storage and cors

--- backend/routes/createUser.js
+++ backend/routes/createUser.js
@@ -1,24 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/userModel'); // Adjust the path as needed
+const bcrypt = require('bcrypt');
 
 // Route to create a user
 router.post('/createUser', (req, res) => {
   const { username, password } = req.body;
 
-  const newUser = new User({
-    username,
-    password,
-  });
+  bcrypt.hash(password, 10, (err, hashedPassword) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ error: 'Could not create user.' });
+    }
 
-  newUser
-    .save()
-    .then((savedUser) => {
-      res.status(201).json(savedUser);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: 'Could not create user.' });
+    const newUser = new User({
+      username,
+      password: hashedPassword, // Store the hashed password
     });
+
+    newUser
+      .save()
+      .then((savedUser) => {
+        res.status(201).json(savedUser);
+      })
+      .catch((error) => {
+        res.status(500).json({ error: 'Could not create user.' });
+      });
+  });
 });
 
 module.exports = router;
